73d38802ce9df364aa8f854edbb0b473f4b5e0ad
Zhanna Bohan <foto1drug@gmail.com>
2023-02-05 15:38:10 +0300

    feat: add controllers for users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpCode, Get, Post, Put, Delete } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from '../interface/interface';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  @HttpCode(200)
+  async getAllUsers(): Promise<User[] | string> {
+    return await this.usersService.getAll();
+  }
+
+  @Get(':id')
+  @HttpCode(200)
+  async getUserById(id: string): Promise<User | string> {
+    return await this.usersService.getById(id);
+  }
+
+  @Post()
+  @HttpCode(201)
+  async createNewUser() {
+    return await this.usersService.createUser();
+  }
+
+  @Put()
+  @HttpCode(200)
+  async updateUserPassword() {
+    return await this.usersService.updatePassword();
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  async deleteUserById(id: string) {
+    return await this.usersService.deleteUser(id);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  getAll() {
+    return `return all user`;
+  }
+  getById(id: string) {
+    return `return user with id = ${id}`;
+  }
+
+  createUser() {
+    return `create new user`;
+  }
+
+  updatePassword() {
+    return `update users password`;
+  }
+
+  deleteUser(id: string) {
+    return `delete user with id = ${id}`;
+  }
+}
