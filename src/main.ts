4ca25605e2e0b3575210dd9c173c6602f7eff485
Zhanna Bohan <foto1drug@gmail.com>
2023-02-06 22:03:25 +0300

    feat: add swagger, fix api.yaml

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
+import { SwaggerModule } from '@nestjs/swagger';
+import { parse } from 'yaml';
+import { readFile } from 'fs/promises';
 
 const HTTP_PORT = Number(process.env.PORT) || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const document = await readFile('./doc/api.yaml', { encoding: 'utf-8' });
+  SwaggerModule.setup('doc', app, parse(document));
+
   await app.listen(HTTP_PORT, () => {
     console.log(`Server has been started on port: ${HTTP_PORT}`);
   });
